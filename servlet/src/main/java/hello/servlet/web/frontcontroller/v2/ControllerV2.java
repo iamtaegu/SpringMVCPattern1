7f2a6832bd7c7d1c2a8ac78a61cda8d3e0a7adf1
Taegu <gmdals45@gmail.com>
2022-03-06 16:45:12 +0900

    2022/03/06, FrontController

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v2/ControllerV2.java
@@ -0,0 +1,16 @@
+package hello.servlet.web.frontcontroller.v2;
+
+import hello.servlet.web.frontcontroller.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV2 {
+    /**
+     * V1은 Controller가 view forward까지 처리
+     * V2은 view forward을 frontController에서 처리
+     */
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -0,0 +1,52 @@
+package hello.servlet.web.frontcontroller.v2;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+public class FrontControllerServletV2 extends HttpServlet {
+
+    // 다형성
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    // 매핑 정보
+    public FrontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("FrontControllerServlerV1.service");
+
+        String requestURI = req.getRequestURI();
+        System.out.println("requestURI= " + requestURI);
+
+        ControllerV2 controller = controllerMap.get(requestURI);
+        if (controller == null) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        // View 분리
+        // process의 결과가 MyView 객체를 반환
+        MyView myView = controller.process(req, resp);
+        myView.render(req, resp);
+    }
+}
+
