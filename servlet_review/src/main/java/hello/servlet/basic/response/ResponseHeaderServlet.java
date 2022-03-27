12bee83778e211feb8134cc15323576cfb43b0c9
Taegu <gmdals45@gmail.com>
2022-03-27 16:41:54 +0900

    2022/03/27, FrontControllerServletV3

---
+++ servlet_review/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,48 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.setHeader("Content-Type", "text/plain:charset=utf-8");
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("my-header", "hello");
+
+        //[Header 편의 메서드]
+        content(resp);
+        cookie(resp);
+        redirect(resp);
+
+        //[message body]
+        PrintWriter writer = resp.getWriter();
+        writer.println("ok");
+    }
+
+    private void redirect(HttpServletResponse resp) throws IOException {
+        resp.sendRedirect("/basic/hello-form.html");
+    }
+
+    private void cookie(HttpServletResponse resp) {
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);
+        resp.addCookie(cookie);
+    }
+
+    private void content(HttpServletResponse resp) {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("euc-kr");
+    }
+}
