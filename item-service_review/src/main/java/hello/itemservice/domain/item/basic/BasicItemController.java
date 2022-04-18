09d5a593739ed5da1a238919e48bf25fac57cf27
Taegu <gmdals45@gmail.com>
2022-04-18 21:29:25 +0900

    2022/04/18 상품 등록(~addItemV1)

--- item-service_review/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
+++ item-service_review/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -5,8 +5,7 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -23,6 +22,48 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    /**
+     * PathVariable로 넘어온 상품ID로 상품을 조회하고
+     * 모델에 담아두고
+     * 뷰 템플릿 호출
+     */
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /*
+    * 상품 등록 폼은 단순히 뷰 템플릿만 호출
+    * */
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    /*
+    * @RequestParam으로 요청 파라미터 값을 가져오고
+    * Item 객체를 생성하고, itemRepository를 통해 저장
+    * item을 모델에 담아 뷰에 전달
+    * */
+    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
     }
 
     /**
