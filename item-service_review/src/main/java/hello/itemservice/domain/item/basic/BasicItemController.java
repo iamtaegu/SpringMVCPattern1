b43d13d4a791e371b5f02e802a0bf2a9dcb0479b
Taegu <gmdals45@gmail.com>
2022-04-27 21:36:25 +0900

    2022/04/27, 스프링MVC 웹 어플리케이션(조회/등록/수정 기능)

--- item-service_review/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
+++ item-service_review/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -3,9 +3,11 @@ package hello.itemservice.domain.item.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -49,7 +51,7 @@ public class BasicItemController {
     * Item 객체를 생성하고, itemRepository를 통해 저장
     * item을 모델에 담아 뷰에 전달
     * */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV1(@RequestParam String itemName,
                             @RequestParam int price,
                             @RequestParam Integer quantity,
@@ -65,6 +67,67 @@ public class BasicItemController {
 
         return "basic/item";
     }
+
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가
+        return "basic/item";
+    }
+
+    // item 이름 생략
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 전체 생략
+     * */
+    //@PostMapping("/add")
+    public String addItemV4(Item item) {
+        System.out.println("[addItemV4]");
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * PRG - Post/Redirect/Get
+     * 상품 등록 처리 이후에 뷰 템플릿이 아니라 상품 상세 화면으로 리다이렉트 시키는 방어 코드 작성
+     * */
+    //@PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    /***
+     * PRG 패턴 적용 이후에 쿼리스트링으로 파라미터를 전달하는 취약점 존재
+     * RedirectAttribute 으로 조치
+     * URL 인코딩도 해주고, pathVariable({itemId} 쿼리 파라미터까지 처리 해줌
+     */
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
 
     /**
      * 테스트용 데이터 추가
